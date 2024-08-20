578d6ab58dca120b7ccf8ac6852a3e669a6e5d33
Taurai Nyamakura <tnyamakura@Taurais-MBP-2.home>
2024-08-20 19:02:19 +0200

    Add Test controller

---
+++ otel-support-samples/otel-support-sample-spring-boot-3.x/src/test/java/io/truemark/otel/support/sample/SampleApplicationTest.java
@@ -0,0 +1,19 @@
+package io.truemark.otel.support.sample;
+
+import io.truemark.otel.support.sample.controllers.TestController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTest {
+
+    @Autowired
+    private TestController testController;
+
+    @Test
+    void test() {
+        Assertions.assertNotNull(testController.test());
+    }
+}
